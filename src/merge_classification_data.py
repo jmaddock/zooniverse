1f9379c137c8992dda82b63ff67d6db24d79f10c
Jim Maddock <skimaddster@gmail.com>
2017-05-30 07:40:39 -0500

    refactored merge classification script

--- src/merge_classification_data.py
+++ src/merge_classification_data.py
@@ -1,130 +1,75 @@
-
-# coding: utf-8
-
-# In[2]:
-
-get_ipython().magic('matplotlib inline')
-
 import pandas as pd
-import matplotlib.pyplot as plt
-import matplotlib
 import numpy as np
-matplotlib.style.use('ggplot')
-
 import requests
 import json
+import utils
+import argparse
 
+def load_panoptes_data(infile,project_df):
+    utils.log('loading file: {0}'.format(infile))
+    # read csv file of panoptes classifications
+    panoptes_classification_df = pd.read_csv(infile, dtype={'user_id': object})
+    # convert 'created_at' to datetime object
+    panoptes_classification_df['created_at'] = pd.to_datetime(panoptes_classification_df['created_at'])
+    # mark all classifications from the panoptes dump
+    panoptes_classification_df['panoptes_dump'] = 1
+    # mark all classifications in the ouroboros dump
+    ouroboros_classification_df['ouroboros_dump'] = 1
+    # mark all classifications in the panoptes API = 1
+    panoptes_classification_df.loc[panoptes_classification_df['project_id'].isin(project_df.loc[project_df['panoptes_api'] == 1]['panoptes_project_id']), 'panoptes_api'] = 1
+    panoptes_classification_df.loc[~panoptes_classification_df['project_id'].isin(project_df.loc[project_df['panoptes_api'] == 1]['panoptes_project_id']), 'panoptes_api'] = 0
+    # drop all non-overlaping columns
+    panoptes_classification_df = panoptes_classification_df[['id', 'project_id', 'user_id', 'created_at', 'panoptes_dump', 'panoptes_api']]
+    # rename columns for consistancy
+    panoptes_classification_df = panoptes_classification_df.rename(columns={'id': 'classification_id', 'project_id': 'panoptes_project_id'})
+    return panoptes_classification_df
 
-# In[79]:
+def load_ouroboros_data(infile,project_df):
+    utils.log('loading file: {0}'.format(infile))
+    # read csv file of ouroboros classifications
+    ouroboros_classification_df = pd.read_csv(infile)
+    # convert 'created_at' to datetime object
+    ouroboros_classification_df['created_at'] = pd.to_datetime(ouroboros_classification_df['created_at'])
+    # rename columns for merging later
+    ouroboros_classification_df = ouroboros_classification_df.rename(columns={'project_id':'ouroboros_mongo_id',
+                                                                              '_id':'classification_id',
+                                                                              'user_name':'user_id'})
+    # merge panoptes project id field with classification df
+    ouroboros_classification_df = ouroboros_classification_df.merge(project_df[['ouroboros_mongo_id', 'panoptes_project_id', 'panoptes_project_name']], on='ouroboros_mongo_id')
+    # mark projects that are in the panoptes api = 1
+    ouroboros_classification_df.loc[ouroboros_classification_df['panoptes_project_id'].isin(project_df.loc[project_df['panoptes_api'] == 1]['panoptes_project_id']), 'panoptes_api'] = 1
+    ouroboros_classification_df.loc[~ouroboros_classification_df['panoptes_project_id'].isin(project_df.loc[project_df['panoptes_api'] == 1]['panoptes_project_id']), 'panoptes_api'] = 0
+    # drop all non-overlapping columns
+    ouroboros_classification_df = ouroboros_classification_df[['_id', 'created_at', 'user_name', 'tutorial', 'panoptes_project_id', 'ouroboros_dump', 'panoptes_api']]
+    return ouroboros_classification_df
 
-# specify data directory and file
-panoptes_data_file = '/srv/zooniverse/tables/panoptes_classification_table_100000.csv'
-# read csv file of panoptes classifications
-panoptes_classification_df = pd.read_csv(panoptes_data_file,dtype={'user_id':object})
-# convert 'created_at' to datetime object
-panoptes_classification_df['created_at'] = pd.to_datetime(panoptes_classification_df['created_at'])
+def merge_and_format(panoptes_classification_df,ouroboros_classification_df):
+    utils.log('appending dfs')
+    # merge classification dfs
+    result_df = ouroboros_classification_df.append(panoptes_classification_df)
+    # change null fields to 0
+    result_df.loc[result_df['panoptes_dump'].isnull(), 'panoptes_dump'] = 0
+    result_df.loc[result_df['ouroboros_dump'].isnull(), 'ouroboros_dump'] = 0
+    return result_df
 
-
-# In[80]:
-
-# specify data directory and file
-ouroboros_data_file = '/srv/zooniverse/tables/ouroboros_classification_table_100000.csv'
-# read csv file of ouroboros classifications
-ouroboros_classification_df = pd.read_csv(ouroboros_data_file)
-# convert 'created_at' to datetime object
-ouroboros_classification_df['created_at'] = pd.to_datetime(ouroboros_classification_df['created_at'])
-
-
-# In[81]:
-
-# specify project table file
-project_table_file = '/srv/zooniverse/tables/all_projects_table_05-19-17.csv'
-# load project table
-project_df = pd.read_csv(project_table_file)
-
-
-# In[82]:
-
-# mark all classifications from the panoptes dump
-panoptes_classification_df['panoptes_dump'] = 1
-# mark all classifications in the ouroboros dump
-ouroboros_classification_df['ouroboros_dump'] = 1
-# make a df containing only projects found in the API
-api_projects = project_df.loc[project_df['panoptes_api'] == 1]
-
-
-# In[83]:
-
-# mark all classifications in the panoptes API == 1
-panoptes_classification_df.loc[panoptes_classification_df['project_id'].isin(api_projects['panoptes_project_id']),'panoptes_api'] = 1
-panoptes_classification_df.loc[~panoptes_classification_df['project_id'].isin(api_projects['panoptes_project_id']),'panoptes_api'] = 0
-
-
-# In[84]:
-
-print(len(panoptes_classification_df.loc[panoptes_classification_df['panoptes_api'] == 1]))
-print(len(panoptes_classification_df.loc[panoptes_classification_df['panoptes_api'] == 0]))
-assert(len(panoptes_classification_df.loc[panoptes_classification_df['panoptes_api'] == 1]) + len(panoptes_classification_df.loc[panoptes_classification_df['panoptes_api'] == 0]) == len(panoptes_classification_df))
-
-
-# In[85]:
-
-ouroboros_classification_df = ouroboros_classification_df.rename(columns={'project_id':'ouroboros_mongo_id'})
-
-
-# In[86]:
-
-ouroboros_classification_df = ouroboros_classification_df.merge(project_df[['ouroboros_mongo_id','panoptes_project_id','panoptes_project_name']],on='ouroboros_mongo_id')
-
-
-# In[87]:
-
-ouroboros_classification_df.loc[ouroboros_classification_df['panoptes_project_id'].isin(api_projects['panoptes_project_id']),'panoptes_api'] = 1
-ouroboros_classification_df.loc[~ouroboros_classification_df['panoptes_project_id'].isin(api_projects['panoptes_project_id']),'panoptes_api'] = 0
-
-
-# In[88]:
-
-print(len(ouroboros_classification_df.loc[ouroboros_classification_df['panoptes_api'] == 0]))
-print(len(ouroboros_classification_df.loc[ouroboros_classification_df['panoptes_api'] == 1]))
-assert(len(ouroboros_classification_df.loc[ouroboros_classification_df['panoptes_api'] == 1]) + len(ouroboros_classification_df.loc[ouroboros_classification_df['panoptes_api'] == 0]) == len(ouroboros_classification_df))
-
-
-# In[89]:
-
-panoptes_classification_df = panoptes_classification_df[['id', 'project_id', 'user_id','created_at','panoptes_dump','panoptes_api']]
-panoptes_classification_df = panoptes_classification_df.rename(columns={'id':'classification_id','project_id':'panoptes_project_id'})
-
-
-# In[90]:
-
-ouroboros_classification_df = ouroboros_classification_df[['_id','created_at','user_name','tutorial','panoptes_project_id','ouroboros_dump','panoptes_api']]
-ouroboros_classification_df = ouroboros_classification_df.rename(columns={'_id':'classification_id','user_name':'user_id'})
-
-
-# In[91]:
-
-result_df = ouroboros_classification_df.append(panoptes_classification_df)
-
-
-# In[96]:
-
-print(len(result_df.loc[result_df['panoptes_dump'].isnull(),'panoptes_dump']))
-print(len(result_df.loc[result_df['panoptes_dump'].notnull(),'panoptes_dump']))
-
-
-# In[97]:
-
-result_df.loc[result_df['panoptes_dump'].isnull(),'panoptes_dump'] = 0
-result_df.loc[result_df['ouroboros_dump'].isnull(),'ouroboros_dump'] = 0
-
-
-# In[98]:
-
-result_df
-
-
-# In[ ]:
-
-
-
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='import a directory of bson dumps to mongo')
+    parser.add_argument('-o','--outfile',
+                        help='output directory path')
+    parser.add_argument('--ouroboros',
+                        help='a csv of ouroboros projects created from a mongo dump')
+    parser.add_argument('--panoptes',
+                        help='a csv of panoptes classifications')
+    parser.add_argument('--project',
+                        help='a csv of projects from both ouroboros and panoptes')
+    args = parser.parse_args()
+    # load project table
+    project_df = pd.read_csv(args.project)
+    panoptes_classification_df = load_panoptes_data(infile=args.panoptes,
+                                                    project_df=project_df)
+    ouroboros_classification_df = load_ouroboros_data(infile=args.ouroboros,
+                                                      project_df=project_df)
+    result_df = merge_and_format(panoptes_classification_df=panoptes_classification_df,
+                                 ouroboros_classification_df=ouroboros_classification_df)
+    utils.log('writing file: {0}'.format(args.outfile))
+    result_df.to_csv(args.outfile,index=False)
